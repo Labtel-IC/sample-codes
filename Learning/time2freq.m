c0ab9828b0876f8e08fbe8f90a1439009e7fe11a
Labtel-IC <56411059+Labtel-IC@users.noreply.github.com>
2019-10-18 14:31:51 -0300

    Rename time2freq.m to Learning/time2freq.m

--- Learning/time2freq.m
+++ Learning/time2freq.m
@@ -2,13 +2,13 @@ function freq = time2freq(T)
 %
 %           function freq = time2freq(T);
 %
-%   Gera o vetor de freqüência para um dado vetor tempo para ser usado junto com a FFT.
+%   Gera o vetor de freqÃ¼Ãªncia para um dado vetor tempo para ser usado junto com a FFT.
 %
 %   ENTRADA:
 %       T            : Vetor tempo
 %
-%   SAÍDA:
-%       freq         : Vetor freqüência
+%   SAÃDA:
+%       freq         : Vetor freqÃ¼Ãªncia
 
 dT    = mean(diff(T));
 nT    = length(T);
